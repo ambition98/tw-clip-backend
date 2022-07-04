cf865c20f124a82a3490a4375245390022a96c7c
YJ_Lee <ambition65@naver.com>
2022-07-04 20:48:13 +0900

    Test Jenkins for Auto build & Deploy

--- src/main/java/com/isedol_clip_backend/IsedolClipBackendApplication.java
+++ src/main/java/com/isedol_clip_backend/IsedolClipBackendApplication.java
@@ -9,5 +9,5 @@ public class IsedolClipBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(IsedolClipBackendApplication.class, args);
     }
-
+    // test jenkins
 }
